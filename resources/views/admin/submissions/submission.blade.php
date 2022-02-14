672e4f5126c4116f962f4cdb44f73e886c6502b5
Uri <45401276+preimpression@users.noreply.github.com>
2022-02-14 15:35:18 -0500

    refactor

--- resources/views/admin/submissions/submission.blade.php
+++ resources/views/admin/submissions/submission.blade.php
@@ -164,8 +164,8 @@
                             {!! Form::text('slug[]', null, ['class' => 'form-control character-code']) !!}
                         </div>
                         <div class="form-group col-6">
-                            {!! Form::label('is_focus[]', 'Focus Character?', ['class' => 'form-check-label'] ) !!}
-                            {!! Form::select('is_focus[]', [0 => 'No' , 1 => 'Yes' ], 0, ['class' => 'form-control']) !!}
+                            {!! Form::label('character-is-focus[]', 'Focus Character?', ['class' => 'form-check-label '] ) !!}
+                            {!! Form::select('character-is-focus[]', [0 => 'No' , 1 => 'Yes' ], 0, ['class' => 'form-control character-is-focus']) !!}
                         </div>
                         <div class="character-rewards hide">
                             <h4>Character Rewards</h4>
