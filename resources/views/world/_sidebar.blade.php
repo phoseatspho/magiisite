808a54533481628ce62feae8f7d05b1180a5536f
Jess Brannon <jessbrannon@icloud.com>
2023-10-07 23:52:58 +0100

    fixing stuffs

--- resources/views/world/_sidebar.blade.php
+++ resources/views/world/_sidebar.blade.php
@@ -22,7 +22,8 @@
         <div class="sidebar-item"><a href="{{ url('world/'. __('awards.award') .'-categories') }}" class="{{ set_active('world/'. __('awards.award') .'-categories*') }}">{{ ucfirst(__('awards.award')) }} Categories</a></div>
         <div class="sidebar-item"><a href="{{ url('world/'. __('awards.awards')) }}" class="{{ set_active('world/'. __('awards.awards') .'*') }}">All {{ ucfirst(__('awards.awards')) }} </a></div>
     </li>
-</ul>
+
+    <li class="sidebar-section">
         <div class="sidebar-section-header">Levels</div>
         <div class="sidebar-item"><a href="{{ url('world/levels/user') }}" class="{{ set_active('world/levels/user*') }}">User Levels</a></div>
         <div class="sidebar-item"><a href="{{ url('world/levels/character') }}" class="{{ set_active('world/levels/character*') }}">Character Levels</a></div>
@@ -35,7 +36,8 @@
         <div class="sidebar-item"><a href="{{ url('world/gear-categories') }}" class="{{ set_active('world/gear-categories*') }}">Gear Categories</a></div>
         <div class="sidebar-item"><a href="{{ url('world/gear') }}" class="{{ set_active('world/gear*') }}">All Gear</a></div>
     </li>
-</ul>
+
+    <li class="sidebar-section">
         <div class="sidebar-section-header">Recipes</div>
         <div class="sidebar-item"><a href="{{ url('world/recipes') }}" class="{{ set_active('world/recipes*') }}">All Recipes</a></div>
     </li>
