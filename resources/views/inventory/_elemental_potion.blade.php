ecbbc12b1bec49360c71d6a609cf32d02ff8a95f
Ne-wt <Ne-wt@users.noreply.github.com>
2023-11-10 17:07:54 +0000

    refactor: fix blade formatting

--- resources/views/inventory/_elemental_potion.blade.php
+++ resources/views/inventory/_elemental_potion.blade.php
@@ -1,11 +1,16 @@
-@if($stack->first()->user_id == Auth::user()->id)
+@if ($stack->first()->user_id == Auth::user()->id)
     <li class="list-group-item">
         <a class="card-title h5 collapse-title" data-toggle="collapse" href="#openBoxForm"> Consume Potion</a>
         <div id="openBoxForm" class="collapse">
             {!! Form::hidden('tag', $tag->tag) !!}
             <p><b>Select a Character to apply this potion to:</b></p>
             <div class="form-group">
-                {!! Form::select('character_id', Auth::user()->characters()->get()->pluck('fullName', 'id'), null, ['class' => 'form-control']) !!}
+                {!! Form::select(
+                    'character_id',
+                    Auth::user()->characters()->get()->pluck('fullName', 'id'),
+                    null,
+                    ['class' => 'form-control'],
+                ) !!}
             </div>
             <p>This action is not reversible. Are you sure you want to use this potion?</p>
             <div class="text-right">
