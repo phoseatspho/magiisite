550680dc817430d780aabaa16c1155eba18f7432
phos <phosph0rescent@hotmail.com>
2023-12-30 14:37:54 +0000

    update site name

--- config/lorekeeper/settings.php
+++ config/lorekeeper/settings.php
@@ -36,7 +36,7 @@ return [
     | site wherever the name needs to be displayed.
     |
     */
-    'site_name'                                         => 'Lorekeeper',
+    'site_name'                                         => 'Magii',
 
     /*
     |--------------------------------------------------------------------------
@@ -48,7 +48,7 @@ return [
     | It is not, however, displayed on the site itself. This should be kept short and snappy!
     |
     */
-    'site_desc'                                         => 'A Lorekeeper ARPG',
+    'site_desc'                                         => 'A magiical ARPG adventure!',
 
     /*
     |--------------------------------------------------------------------------
