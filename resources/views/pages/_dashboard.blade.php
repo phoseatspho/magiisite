a14455fd2cd6e5425feec58e1ba80dc3eb131e79
phos <phosph0rescent@hotmail.com>
2023-12-31 21:50:52 +0000

    no message

--- resources/views/pages/_dashboard.blade.php
+++ resources/views/pages/_dashboard.blade.php
@@ -7,51 +7,52 @@
  <img src="../files/banner.png" class="img-fluid">
  <br> 
 
+ <div class="dashboardtext">
 <div class="row justify-content-center">
- <div class="col-6 col-md-3">
-<div class="dashboardtext">
+<div class="col-6 col-md-3">
+ 
  <h3>Profile</h3>
- <ul>
+<ul>
  <img src="../files/profile.png" class="img-fluid">
  <li><a href="https://magiispecies.com/profile">Profile</a></li>
  <li><a href="https://magiispecies.com/characters">My Magii</a></li>
  <li><a href="https://magiispecies.com/inventory">Inventory</a></li>
  <li><a href="https://magiispecies.com/bank">Bank</a></li>
- </ul>
- </div>
+</ul>
+</div>
 
- <div class="col-6 col-md-3">
+<div class="col-6 col-md-3">
  <h3>Info</h3>
- <ul>
+<ul>
  <img src="../files/explore.png" class="img-fluid">
  <li><a href="https://magiispecies.com/profile">Profile</a></li>
  <li><a href="https://magiispecies.com/characters">My Magii</a></li>
  <li><a href="https://magiispecies.com/inventory">Inventory</a></li>
  <li><a href="https://magiispecies.com/bank">Bank</a></li>
- </ul>
- </div>
+</ul>
+</div>
 
- <div class="col-6 col-md-3">
+<div class="col-6 col-md-3">
  <h3>Play</h3>
- <ul>
+<ul>
   <img src="../files/play.png" class="img-fluid"> 
   <li><a href="https://magiispecies.com/profile">Profile</a></li>
  <li><a href="https://magiispecies.com/characters">My Magii</a></li>
  <li><a href="https://magiispecies.com/inventory">Inventory</a></li>
  <li><a href="https://magiispecies.com/bank">Bank</a></li>
- </ul>
- </div>
+</ul>
+</div>
 
- <div class="col-6 col-md-3">
+<div class="col-6 col-md-3">
   <h3>Explore</h3>
-  <ul>
+<ul>
   <img src="../files/explore.png" class="img-fluid">
   <li><a href="https://magiispecies.com/profile">Profile</a></li>
  <li><a href="https://magiispecies.com/characters">My Magii</a></li>
  <li><a href="https://magiispecies.com/inventory">Inventory</a></li>
  <li><a href="https://magiispecies.com/bank">Bank</a></li>
- </ul>
- </div>
+</ul>
+</div>
 </div> 
 </div>
 
