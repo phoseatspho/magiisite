915a83cc968b10a681b53ad0262d5276c4c9326e
Jess Brannon <jessbrannon@icloud.com>
2024-01-12 18:50:42 +0000

    no message

--- resources/views/worldexpansion/world.blade.php
+++ resources/views/worldexpansion/world.blade.php
@@ -9,6 +9,7 @@
 
 <div class="site-page-content parsed-text">
 <img src="../files/lorescroll.png" class="img-fluid">
+<img src="../files/mapsprite.png" class="img-fluid">
     {!! $world->parsed_text !!}
 </div>
 
