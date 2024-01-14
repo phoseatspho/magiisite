a4fdf83d826ace9058641bf756f11189848ca66c
Jess Brannon <jessbrannon@icloud.com>
2024-01-14 23:09:37 +0000

    no message

--- resources/views/worldexpansion/world.blade.php
+++ resources/views/worldexpansion/world.blade.php
@@ -6,14 +6,29 @@
 {!! breadcrumbs(['World' => 'world', $world->title => 'world/info']) !!}
 
 <h1>Explore Arcadia</h1>
-
+<div class="row justify-content-center">
 <div class="site-page-content parsed-text">
+<div class="col-6 col-md-3">
+    <h3>Lore</h3>
 <img src="../files/lorescroll.png" class="img-fluid">
+</div>
+<div class="col-6 col-md-3">
+    <h3>Locations</h3>
 <img src="../files/mapsprite.png" class="img-fluid">
-<img src="../files/flora.png" class="img-fluid">
-<img src="../files/fauna.png" class="img-fluid">
+</div>
+<div class="col-6 col-md-3">
+    <h3>Npcs</h3>
 <img src="../files/npcs.png" class="img-fluid">
+</div>
+<div class="col-6 col-md-3">
+    <h3>Flora</h3>
+<img src="../files/flora.png" class="img-fluid">
+</div>
+<div class="col-6 col-md-3">
+    <h3>Fauna</h3>
+<img src="../files/fauna.png" class="img-fluid">
     {!! $world->parsed_text !!}
+</div>
 </div>
 
 @endsection
