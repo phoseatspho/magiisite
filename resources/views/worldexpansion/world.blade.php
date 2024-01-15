b80dc623b0387c3db8ea95d805b9dc677ec512aa
Jess Brannon <jessbrannon@icloud.com>
2024-01-15 10:42:33 +0000

    no message

--- resources/views/worldexpansion/world.blade.php
+++ resources/views/worldexpansion/world.blade.php
@@ -8,13 +8,14 @@
 <h1>Explore Arcadia</h1>
 <div class="site-page-content parsed-text">
 <div align="center">
-<img src="../files/lorescroll.png" class="img-fluid" title="Lore">
-<img src="../files/mapsprite.png" class="img-fluid">
-<img src="../files/npcs.png" class="img-fluid">
-<img src="../files/flora.png" class="img-fluid">
-<img src="../files/fauna.png" class="img-fluid">
+<a href="https://www.magiispecies.com/world/concept-categories" img src="../files/lorescroll.png" class="img-fluid" title="Lore"></a>
+<a href="https://www.magiispecies.com/world/location-types" img src="../files/mapsprite.png" class="img-fluid" title="Locations"></a>
+<a href="https://www.magiispecies.com/world/figure-categories" img src="../files/npcs.png" class="img-fluid" title="NPCs"></a>
+<a href="https://www.magiispecies.com/world/flora-categories" img src="../files/flora.png" class="img-fluid" title="Flora"></a>
+<a href="https://www.magiispecies.com/world/fauna-categories" img src="../files/fauna.png" class="img-fluid" title="Fauna"></a>
 </div>
-    {!! $world->parsed_text !!}
+
+Click the sprites above to read more about the World of Arcadia and its inhabitants!
 </div>
 
 
