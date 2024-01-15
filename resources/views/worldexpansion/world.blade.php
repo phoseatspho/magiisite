7c58357ad4e5cd2e141a036d597470845b79d324
Jess Brannon <jessbrannon@icloud.com>
2024-01-15 10:45:29 +0000

    no message

--- resources/views/worldexpansion/world.blade.php
+++ resources/views/worldexpansion/world.blade.php
@@ -12,7 +12,7 @@
 <a href="https://www.magiispecies.com/world/figure-categories"><img src="../files/npcs.png" class="img-fluid" title="NPCs"></a>
 <a href="https://www.magiispecies.com/world/flora-categories"><img src="../files/flora.png" class="img-fluid" title="Flora"></a>
 <a href="https://www.magiispecies.com/world/fauna-categories"><img src="../files/fauna.png" class="img-fluid" title="Fauna"></a>
-
+<br>
 Click the sprites above to read more about the World of Arcadia and its inhabitants!
 </div>
 
