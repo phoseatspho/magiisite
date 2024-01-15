ec4eb51c99a662b947415bc1a80ee8980e6cd1b3
Jess Brannon <jessbrannon@icloud.com>
2024-01-15 10:33:57 +0000

    no message

--- resources/views/worldexpansion/world.blade.php
+++ resources/views/worldexpansion/world.blade.php
@@ -7,12 +7,13 @@
 
 <h1>Explore Arcadia</h1>
 <div class="site-page-content parsed-text">
-
+<div align="center">
 <img src="../files/lorescroll.png" class="img-fluid">
 <img src="../files/mapsprite.png" class="img-fluid">
 <img src="../files/npcs.png" class="img-fluid">
 <img src="../files/flora.png" class="img-fluid">
 <img src="../files/fauna.png" class="img-fluid">
+</div>
     {!! $world->parsed_text !!}
 </div>
 
