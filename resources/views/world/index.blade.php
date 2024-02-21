2117650818b2f2c7fb5dab3e32545a4a8b9f3718
Jess Brannon <jessbrannon@icloud.com>
2024-02-21 21:03:03 +0000

    no message

--- resources/views/world/index.blade.php
+++ resources/views/world/index.blade.php
@@ -11,67 +11,67 @@
 <div class="row justify-content-center">
 <div class="col-6 col-md-3">
  
- <h4>Species</h4>
+ <h5>Species</h5>
  <a href="https://www.magiispecies.com/world/species"><img src="../files/species.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Elements</h4>
+ <h5>Elements</h5>
  <a href="{{ url('world/elements') }}"><img src="../files/elements.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Currency</h4>
+ <h5>Currency</h5>
  <a href="{{ url('world/currencies') }}"><img src="../files/currencies.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Items</h4>
+ <h5>Items</h5>
  <a href="{{ url('world/items') }}"><img src="../files/items.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Awards</h4>
+ <h5>Awards</h5>
  <a href="{{ url('world/'.__('awards.awards')) }}"><img src="../files/awards.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Collections</h4>
+ <h5>Collections</h5>
  <a href="{{ url('world/collections') }}"><img src="../files/collections.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Recipes</h4>
+ <h5>Recipes</h5>
  <a href="{{ url('world/recipes') }}"><img src="../files/recipes.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Pets</h4>
+ <h5>Pets</h5>
  <a href="{{ url('world/pets') }}"><img src="../files/pets.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Weapons</h4>
+ <h5>Weapons</h5>
  <a href="{{ url('world/weapons') }}"><img src="../files/weapons.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Equipment</h4>
+ <h5>Equipment</h5>
  <a href="{{ url('world/gear') }}"><img src="../files/equipment.png" class="img-fluid"></a>
 </div>
 
 <div class="col-6 col-md-3">
  
- <h4>Skills</h4>
+ <h5>Skills</h5>
  <a href="{{ url('world/skills') }}"><img src="../files/skills.png" class="img-fluid"></a>
 </div>
 </div> 
