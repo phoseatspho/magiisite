4b260c08c8294ef34fd962c628998ae85cba177b
Jess Brannon <jessbrannon@icloud.com>
2024-02-23 15:58:13 +0000

    no message

--- resources/views/pages/_dashboard.blade.php
+++ resources/views/pages/_dashboard.blade.php
@@ -13,13 +13,13 @@
 <div class="row justify-content-center">
 <div class="col-6 col-md-3">
  
- <h2>Profile</h2>
+ <h2>&nbsp;Profile</h2>
 <ul>
  <img src="../files/profile.png" class="img-fluid">
- <h6><li><a href="{{ Auth::user()->url }}">Profile</a></li>
- <li><a href="https://magiispecies.com/characters">My Magii</a></li>
- <li><a href="https://magiispecies.com/inventory">Inventory</a></li>
- <li><a href="https://magiispecies.com/bank">Bank</a></li></h6>
+ <li style="font-size:18px"><a href="{{ Auth::user()->url }}">Profile</a></li>
+ <li style="font-size:18px"><a href="https://magiispecies.com/characters">My Magii</a></li>
+ <li style="font-size:18px"><a href="https://magiispecies.com/inventory">Inventory</a></li>
+ <li><a href="https://magiispecies.com/bank">Bank</a></li>
 </ul>
 </div>
 
@@ -27,10 +27,10 @@
  <h2>Info</h2>
 <ul>
  <img src="../files/info.png" class="img-fluid">
- <h5><li><a href="https://magiispecies.com/profile">Beginner Guide</a></li>
- <li><a href="https://magiispecies.com/characters">Species Info</a></li>
- <li><a href="https://magiispecies.com/inventory">Origin Lore</a></li>
- <li><a href="{{ url('faq') }}">FAQ</a></li></h5>
+ <li style="font-size:18px"><a href="https://magiispecies.com/profile">Beginner Guide</a></li>
+ <li style="font-size:18px"><a href="https://magiispecies.com/characters">Species Info</a></li>
+ <li style="font-size:18px"><a href="https://magiispecies.com/inventory">Origin Lore</a></li>
+ <li style="font-size:18px"><a href="{{ url('faq') }}">FAQ</a></li>
 </ul>
 </div>
 
@@ -38,10 +38,10 @@
  <h2>Play</h2>
 <ul>
   <img src="../files/play.png" class="img-fluid"> 
-  <li style="font-size:20px"><a href="https://www.magiispecies.com/dailies">Check In</a></li>
- <li><a href="https://www.magiispecies.com/prompts/prompts">Quests</a></li>
- <li><a href="https://www.magiispecies.com/foraging">Foraging</a></li>
- <li><a href="https://www.magiispecies.com/crafting">Crafting</a></li>
+  <li style="font-size:18px"><a href="https://www.magiispecies.com/dailies">Check In</a></li>
+ <li style="font-size:18px"><a href="https://www.magiispecies.com/prompts/prompts">Quests</a></li>
+ <li style="font-size:18px"><a href="https://www.magiispecies.com/foraging">Foraging</a></li>
+ <li style="font-size:18px"><a href="https://www.magiispecies.com/crafting">Crafting</a></li>
 </ul>
 </div>
 
@@ -49,10 +49,10 @@
   <h2>Explore</h2>
 <ul>
   <img src="../files/explore.png" class="img-fluid">
-  <li><a href="https://www.magiispecies.com/world/info" style="font-size:18px">Arcadia</a></li>
- <li><a href="https://magiispecies.com/characters">Story</a></li>
- <li><a href="https://www.magiispecies.com/world/figure-categories">NPCs</a></li>
- <li><a href="https://www.magiispecies.com/shops">Shop</a></li>
+  <li style="font-size:18px"><a href="https://www.magiispecies.com/world/info">Arcadia</a></li>
+ <li style="font-size:18px"><a href="https://magiispecies.com/characters">Story</a></li>
+ <li style="font-size:18px"><a href="https://www.magiispecies.com/world/figure-categories">NPCs</a></li>
+ <li style="font-size:18px"><a href="https://www.magiispecies.com/shops">Shop</a></li>
 </ul>
 </div>
 </div> 
