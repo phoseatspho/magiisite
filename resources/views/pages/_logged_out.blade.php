079a88f549602b460208e09b0bdd5847b7d5c02d
Jess Brannon <jessbrannon@icloud.com>
2024-03-07 16:44:23 +0000

    no message

--- resources/views/pages/_logged_out.blade.php
+++ resources/views/pages/_logged_out.blade.php
@@ -1,5 +1,9 @@
+why wont this work what da heck pls work
+@include('widgets._news', ['textPreview' => true])
+
 <h1>{{ config('lorekeeper.settings.site_name', 'Lorekeeper') }}</h1>
 
-{!! $about->parsed_text !!
-@include('widgets._news', ['textPreview' => true])
-}
+{!! $about->parsed_text !!}
+
+test test helloo 
+
