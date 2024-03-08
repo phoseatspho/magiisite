3d70102320e68c1f0489dac28da7d315769abda1
Jess Brannon <jessbrannon@icloud.com>
2024-03-08 16:33:51 +0000

    no message

--- app/Models/Border/Border.php
+++ app/Models/Border/Border.php
@@ -270,7 +270,7 @@ class Border extends Model
 
         //then over style
         } else {
-            return '<div style="width:125px; height:125px;border-radius:50%;">
+            return '<div style="width:135px; height:135px;border-radius:50%;">
                     <!-- frame -->
                     <img src="' .
                 $this->imageUrl .
