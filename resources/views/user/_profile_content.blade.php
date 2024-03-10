53d996eba9ab88c4aa4f1d2022728ac1935481c8
Jess Brannon <jessbrannon@icloud.com>
2024-03-10 22:19:48 +0000

    no message

--- resources/views/user/_profile_content.blade.php
+++ resources/views/user/_profile_content.blade.php
@@ -5,7 +5,7 @@
 <div class="row mb-3">
     <div class="col-md-2 text-center">
         <!-- User Icon -->
-       
+        {!! $user->userBorder !!}
     </div>
 
     <div class="col">
@@ -13,7 +13,7 @@
         <div class="row no-gutters">
             <div class="col h2 text-center text-md-left">
              
-        
+            
             {!! $user->displayName !!} 
                 <a href="{{ url('reports/new?url=') . $user->url }}"><i class="fas fa-exclamation-triangle fa-xs" data-toggle="tooltip" title="Click here to report this user." style="opacity: 50%; font-size:0.5em;"></i></a>
             </div>
