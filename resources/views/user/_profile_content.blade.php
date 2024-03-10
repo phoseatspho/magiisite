86a5d92b32ad8912356101a590493ab260d2da91
Jess Brannon <jessbrannon@icloud.com>
2024-03-10 22:15:19 +0000

    no message

--- resources/views/user/_profile_content.blade.php
+++ resources/views/user/_profile_content.blade.php
@@ -5,8 +5,7 @@
 <div class="row mb-3">
     <div class="col-md-2 text-center">
         <!-- User Icon -->
-       
-        <img src="{!! $user->userBorder !!}" class="img-fluid rounded-circle" style="max-height: 125px;" alt="{{ $user->name }}'s Avatar">
+      
     </div>
 
     <div class="col">
