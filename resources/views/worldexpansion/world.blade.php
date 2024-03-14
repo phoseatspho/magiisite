8437ee6a2e16b6616a09d2b16adb028681870399
Jess Brannon <jessbrannon@icloud.com>
2024-03-14 21:55:51 +0000

    no message

--- resources/views/worldexpansion/world.blade.php
+++ resources/views/worldexpansion/world.blade.php
@@ -13,31 +13,31 @@
 </div>
     <div class="dashboardtext">
 <div class="row justify-content-center">
-<div class="col-6 col-md-3">
+<div class="col-6 col-md-2">
  
  <h5>Lore</h5>
  <a href="https://www.magiispecies.com/world/library"><img src="../files/lorescroll.png" class="img-fluid"></a>
 </div>
 
-<div class="col-6 col-md-3">
+<div class="col-6 col-md-2">
  
  <h5>Locations</h5>
  <a href="https://www.magiispecies.com/world/location-types"><img src="../files/mapsprite.png" class="img-fluid"></a>
 </div>
 
-<div class="col-6 col-md-3">
+<div class="col-6 col-md-2">
  
  <h5>NPCS</h5>
  <a href="https://www.magiispecies.com/sublist/NPCs"><img src="../files/npcs.png" class="img-fluid"></a>
 </div>
 
-<div class="col-6 col-md-3">
+<div class="col-6 col-md-2">
  
  <h5>Flora</h5>
  <a href="https://www.magiispecies.com/world/flora-categories"><img src="../files/flora.png" class="img-fluid"></a>
 </div>
 
-<div class="col-6 col-md-3">
+<div class="col-6 col-md-2">
  
  <h5>Fauna</h5>
  <a href="https://www.magiispecies.com/world/fauna-categories"><img src="../files/fauna.png" class="img-fluid"></a>
