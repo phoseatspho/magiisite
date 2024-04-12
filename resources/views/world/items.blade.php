2bbfc7f9fd69c0fdefbd0ba21863e017bb2072b0
Jess Brannon <jessbrannon@icloud.com>
2024-04-12 08:08:18 +0100

    no message

--- resources/views/world/items.blade.php
+++ resources/views/world/items.blade.php
@@ -6,43 +6,41 @@
 
 @section('content')
     {!! breadcrumbs(['World' => 'world', 'Items' => 'world/items']) !!}
-        <h1>Items</h1>
+    <h1>Items</h1>
 
-        <div>
-            {!! Form::open(['method' => 'GET', 'class' => '']) !!}
-            <div class="form-inline justify-content-end">
-                <div class="form-group ml-3 mb-3">
-                    {!! Form::text('name', Request::get('name'), ['class' => 'form-control', 'placeholder' => 'Name']) !!}
-                </div>
-                <div class="form-group ml-3 mb-3">
-                    {!! Form::select('item_category_id', $categories, Request::get('item_category_id'), ['class' => 'form-control']) !!}
-                </div>
-                @if (config('lorekeeper.extensions.item_entry_expansion.extra_fields'))
-    <div class="form-group ml-3 mb-3">
-                        {!! Form::select('artist', $artists, Request::get('artist'), ['class' => 'form-control']) !!}
-                    </div>
-    @endif
+    <div>
+        {!! Form::open(['method' => 'GET', 'class' => '']) !!}
+        <div class="form-inline justify-content-end">
+            <div class="form-group ml-3 mb-3">
+                {!! Form::text('name', Request::get('name'), ['class' => 'form-control', 'placeholder' => 'Name']) !!}
             </div>
-            <div class="form-inline justify-content-end">
-                <div class="form-group ml-3 mb-3">
-                    {!! Form::select(
-                        'sort',
-                        [
-                            'alpha' => 'Sort Alphabetically (A-Z)',
-                            'alpha-reverse' => 'Sort Alphabetically (Z-A)',
-                            'category' => 'Sort by Category',
-                            'newest' => 'Newest First',
-                            'oldest' => 'Oldest First',
-                        ],
-                        Request::get('sort') ?: 'category',
-                        ['class' => 'form-control'],
-                    ) !!}
-                </div>
-                <div class="form-group ml-3 mb-3">
-                    {!! Form::submit('Search', ['class' => 'btn btn-primary']) !!}
-                </div>
+            <div class="form-group ml-3 mb-3">
+                {!! Form::select('item_category_id', $categories, Request::get('item_category_id'), ['class' => 'form-control']) !!}
             </div>
-            {!! Form::close() !!}
+            @if (config('lorekeeper.extensions.item_entry_expansion.extra_fields'))
+                <div class="form-group ml-3 mb-3">
+                    {!! Form::select('artist', $artists, Request::get('artist'), ['class' => 'form-control']) !!}
+                </div>
+            @endif
+        </div>
+        <div class="form-inline justify-content-end">
+            <div class="form-group ml-3 mb-3">
+                {!! Form::select(
+                    'sort',
+                    [
+                        'alpha' => 'Sort Alphabetically (A-Z)',
+                        'alpha-reverse' => 'Sort Alphabetically (Z-A)',
+                        'category' => 'Sort by Category',
+                        'newest' => 'Newest First',
+                        'oldest' => 'Oldest First',
+                    ],
+                    Request::get('sort') ?: 'category',
+                    ['class' => 'form-control'],
+                ) !!}
+            </div>
+            <div class="form-group ml-3 mb-3">
+                {!! Form::submit('Search', ['class' => 'btn btn-primary']) !!}
+            </div>
         </div>
     {!! Form::close() !!}
 </div>
@@ -63,7 +61,7 @@
 
   
     @endforeach
-        {!! $items->render() !!}
+    {!! $items->render() !!}
 
-        <div class="text-center mt-4 small text-muted">{{ $items->total() }} result{{ $items->total() == 1 ? '' : 's' }} found.</div>
+    <div class="text-center mt-4 small text-muted">{{ $items->total() }} result{{ $items->total() == 1 ? '' : 's' }} found.</div>
 @endsection
