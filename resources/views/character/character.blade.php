5c579c73ef3e6d672cd3bb8cc0417f4cb1b9b87d
Jess Brannon <jessbrannon@icloud.com>
2024-04-17 09:52:27 +0100

    no message

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -11,15 +11,15 @@
 @section('profile-content')
 @include('widgets._awardcase_feature', ['target' => $character, 'count' => Config::get('lorekeeper.extensions.awards.character_featured'), 'float' => true])
     @if ($character->is_myo_slot)
-    {!! breadcrumbs(['MYO Slot Masterlist' => 'myos', $character->fullName => $character->url]) !!}
-@else
-    {!! breadcrumbs([
-        $character->category->masterlist_sub_id ? $character->category->sublist->name . ' Masterlist' : 'Character masterlist' => $character->category->masterlist_sub_id ? 'sublist/' . $character->category->sublist->key : 'masterlist',
-        $character->fullName => $character->url,
-    ]) !!}
+        {!! breadcrumbs(['MYO Slot Masterlist' => 'myos', $character->fullName => $character->url]) !!}
+    @else
+        {!! breadcrumbs([
+            $character->category->masterlist_sub_id ? $character->category->sublist->name . ' Masterlist' : 'Character masterlist' => $character->category->masterlist_sub_id ? 'sublist/' . $character->category->sublist->key : 'masterlist',
+            $character->fullName => $character->url,
+        ]) !!}
     @endif
 
-        @include('character._header', ['character' => $character])
+    @include('character._header', ['character' => $character])
 
 @if ($character->images()->where('is_valid', 1)->whereNotNull('transformation_id')->exists())
         <div class="card-header mb-2">
@@ -101,56 +101,9 @@
                         <a href="#" class="btn btn-outline-danger btn-sm delete-character" data-slug="{{ $character->slug }}">Delete</a>
                     </div>
                 </div>
-                @if ($character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)))
-    <div class="text-right">You are viewing the full-size image. <a href="{{ $character->image->imageUrl }}">View watermarked image</a>?</div>
-    @endif
-            </div>
-            @include('character._image_info', ['image' => $character->image])
+            @endif
         </div>
-
-        {{-- Info --}}
-        <div class="card character-bio">
-            <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="statsTab" data-toggle="tab" href="#stats" role="tab">Stats</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="notesTab" data-toggle="tab" href="#notes" role="tab">Description</a>
-                    </li>
-                    @if (Auth::check() && Auth::user()->hasPower('manage_characters'))
-    <li class="nav-item">
-                            <a class="nav-link" id="settingsTab" data-toggle="tab" href="#settings-{{ $character->slug }}" role="tab"><i class="fas fa-cog"></i></a>
-                        </li>
-    @endif
-                </ul>
-            </div>
-            <div class="card-body tab-content">
-                <div class="tab-pane fade show active" id="stats">
-                    @include('character._tab_stats', ['character' => $character])
-                </div>
-                <div class="tab-pane fade" id="notes">
-                    @include('character._tab_notes', ['character' => $character])
-                </div>
-                @if (Auth::check() && Auth::user()->hasPower('manage_characters'))
-    <div class="tab-pane fade" id="settings-{{ $character->slug }}">
-                        {!! Form::open(['url' => $character->is_myo_slot ? 'admin/myo/' . $character->id . '/settings' : 'admin/character/' . $character->slug . '/settings']) !!}
-                        <div class="form-group">
-                            {!! Form::checkbox('is_visible', 1, $character->is_visible, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
-                            {!! Form::label('is_visible', 'Is Visible', ['class' => 'form-check-label ml-3']) !!} {!! add_help('Turn this off to hide the character. Only mods with the Manage Masterlist power (that\'s you!) can view it - the owner will also not be able to see the character\'s page.') !!}
-                        </div>
-                        <div class="text-right">
-                            {!! Form::submit('Edit', ['class' => 'btn btn-primary']) !!}
-                        </div>
-                        {!! Form::close() !!}
-                        <hr />
-                        <div class="text-right">
-                            <a href="#" class="btn btn-outline-danger btn-sm delete-character" data-slug="{{ $character->slug }}">Delete</a>
-                        </div>
-                    </div>
-    @endif
-            </div>
-        </div>
+    </div>
 @endsection
 
 @section('scripts')
